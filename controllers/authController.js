31ce8634d769ecb3648b40ecff9ea8321a9197aa
AmiraDeef <amiradeef4@gmail.com>
2026-05-13 11:46:42 +0300

    test 2 n8n

--- controllers/authController.js
+++ controllers/authController.js
@@ -90,7 +90,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
     try {
  return res.status(200).json({
-            msg: " logout success",token
+            msg: "logout success ",token
         })
     } catch (error) {
         return res.status(500).json({
